dbd23a2080dd6c3536034170106008de93427ccd
winwebcode <45618083+winwebcode@users.noreply.github.com>
2021-04-25 01:12:35 +0700

    Initial commit

---
+++ resources/views/regexp/index.blade.php
@@ -0,0 +1,27 @@
+
+@extends('\layout')
+@section('content')
+    <!-- Begin page content -->
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script src="/js/modal_window.js"></script>
+
+
+    <main class="flex-shrink-0">
+        <div class="container">
+            <h1 class="mt-5">Regexp generator</h1>
+
+            <div class="container">
+                <input type="text" placeholder="Перед искомым текстом всегда есть" name="beforeText">
+                <input type="text" placeholder="Искомый текст всегда начинается с" name="textStart">
+                <input type="text" placeholder="Это идёт после искомого текста" name="afterText">
+                <input type="text" placeholder="Этим заканчивается искомый текст" name="textFinish">
+                <button type="submit">Generate</button>
+            </div>
+
+
+
+        </div>
+    </main>
+@endsection
