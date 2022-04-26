a2daed1dcd60c95352e13122c7b8aaf6c69c72c2
winwebcode <45618083+winwebcode@users.noreply.github.com>
2022-04-26 16:13:16 +0700

    PSR 2
    PSR 2
    удаление лишних комментов

--- app/Models/Regexp.php
+++ app/Models/Regexp.php
@@ -9,13 +9,12 @@ class Regexp extends Model
 {
     use HasFactory;
 
-    public function getRegexp($request)
-    {//generate regexp
-
-        if($request->beforeText == null && $request->textStart == null && $request->textFinish == null && $request->afterText == null) {
+    public function getRegexp($request): string
+    {
+        //generate regexp
+        if ($this->isAnyTextNull($request)) {
             return $regexp = ".+";
-        }
-        else {
+        } else {
             $beforeText = $this->beforeText($request->beforeText); //перед искомым текстом
             $textStart = $this->textStart($request->textStart); //искомый текст
             $textFinish = $this->textFinish($request->textFinish);  //Этим заканчивается искомый текст
@@ -36,40 +35,47 @@ class Regexp extends Model
         }
     }
 
-    public function beforeText($beforeText)
-    {//перед искомым текстом
-        //разбор строки по кускам и замена символов типа слэша
+    public function beforeText($beforeText): string
+    {
+        $result = '';
         $strArray = str_split($beforeText);
-        $result = $this->specCharacterReplacer($strArray); //в цикле проходим по массиву символов, ищем спец. символы и заменяем
+        $result = $this->specCharacterReplacer($strArray);
         return $result = "(?<=$result)";
     }
 
-    public function afterText($afterText)
-    {//после искомого текста
+    public function afterText($afterText): string
+    {
+        //после искомого текста
+        $result = '';
         $strArray = str_split($afterText);
-        $result = $this->specCharacterReplacer($strArray); //в цикле проходим по массиву символов, ищем спец. символы и заменяем
+        $result = $this->specCharacterReplacer($strArray);
 
         return $result = "(?=$result)";
     }
 
-    public function textStart($textStart)
-    {//искомый текст
-        $strArray = str_split($textStart); //string to array
+    public function textStart($textStart): string
+    {
+        //искомый текст
+        $result = '';
+        $strArray = str_split($textStart);
         $result = $this->specCharacterReplacer($strArray);
 
         return $result;
     }
 
-    public function textFinish($textFinish)
-    {//Этим заканчивается искомый текст
+    public function textFinish($textFinish): string
+    {
+        //Этим заканчивается искомый текст
+        $result = '';
         $strArray = str_split($textFinish);
-        $result = $this->specCharacterReplacer($strArray); //в цикле проходим по массиву символов, ищем спец. символы и заменяем
+        $result = $this->specCharacterReplacer($strArray);
 
         return $result;
     }
 
-    public function specCharacterReplacer($strArray)
-    { //replace special character
+    public function specCharacterReplacer($strArray): string
+    {
+        //в цикле проходим по массиву символов, ищем спец. символы и заменяем
         foreach ($strArray as $symbol) {
             // экранирование . ^ $ * + ? { } [ ] \ | ( )
             switch ($symbol) {
@@ -118,7 +124,20 @@ class Regexp extends Model
             }
             $newString[] = $symbol;
         }
-        $newString = implode($newString);
-        return $newString;
+
+        if (isset($newString)) {
+            $newString = implode($newString);
+            return $newString;
+        }
+    }
+
+    public function isAnyTextNull($request): bool
+    {
+        if ($request->beforeText == null && $request->textStart == null &&
+            $request->textFinish == null && $request->afterText == null) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
